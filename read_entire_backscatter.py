0ddfc4c7e94304936da3e829805a61609b020c7c
osobo <olle.berglof@gmail.com>
2019-02-23 09:44:17 +0100

    Add read_entire_backscatter.py

---
+++ read_entire_backscatter.py
@@ -0,0 +1,99 @@
+import sys
+from PIL import Image
+import numpy as np
+import csv
+
+FILE = "backscatter_data/RM30_Aug2013_10cm.csv"
+
+# d = depth
+# s = stdev
+# a = acoustic beam footprint
+# u = unfiltered backscatter strength
+# c = class (sand or whatnot) (but as a int)
+
+CLASS_CMYK = {
+    1: (0, 255, 255, 0), # red    = sand
+    2: (255, 0, 255, 0), # green  = sand/gravel
+    3: (255, 255, 0, 0), # blue   = gravel
+    4: (0, 0, 0, 255),   # black  = sand/boulder/rock
+    5: (0, 0, 255, 0)    # yellow = boulder/rock
+}
+
+RANGES = {
+    'd': (-21.940, -1.500),
+    's': (0.000, 9.143),
+    'a': (0.000, 1.950),
+    'u': (-467.609, -18.322)
+}
+
+#XMIN = 219761.930
+#XMAX = 220293.830
+#YMIN = 612354.274
+#YMAX = 613013.774
+# In decimeters
+XMIN = 2197619
+XMAX = 2202938
+YMIN = 6123542
+YMAX = 6130137
+
+W = 5320
+H = 6596
+
+SETTINGS = {}
+for k in RANGES:
+    lo = float(RANGES[k][0])
+    hi = float(RANGES[k][1])
+    SETTINGS[k] = {'lo': lo, 'hi': hi}
+
+
+def xstr2col(x):
+    return int(float(x)*10) - XMIN
+
+def ystr2row(y):
+    return int(float(y)*10) - YMIN
+
+# Translates feature with range in settings of value x to a color channel
+def raw2cmyk(settings, x):
+    lo = settings['lo']
+    hi = settings['hi']
+    width = hi - lo
+    if x == np.inf:
+        return np.uint8(255)
+    if width < 0.0001:
+        return np.uint8(0)
+    return np.uint8(((x-lo)/width) * 255)
+
+def raws2cmyk(d, s, a, u):
+    # Different orders give different looking images
+    return (
+        raw2cmyk(SETTINGS['a'], a),
+        raw2cmyk(SETTINGS['d'], d),
+        raw2cmyk(SETTINGS['s'], s),
+        raw2cmyk(SETTINGS['u'], u)
+    )
+
+def cat2cmyk(c):
+    return CLASS_CMYK[c]
+
+feats = np.zeros((H,W,4), dtype=np.uint8)
+#cats = np.zeros((H,W,4), dtype=np.uint8)
+
+
+reader = csv.reader(open(FILE, 'r'))
+for line in reader:
+    i = ystr2row(line[1].strip())
+    j = xstr2col(line[0].strip())
+
+    d = float(line[2].strip())
+    s = float(line[3].strip())
+    a = float(line[4].strip())
+    u = float(line[6].strip())
+    #c = int(float(line[7].strip()))
+
+    feats[i,j] = raws2cmyk(d, s, a, u)
+    #cats[i,j] = cat2cmyk(c)
+
+feat_img = Image.fromarray(feats, mode='CMYK')
+#cat_img = Image.fromarray(cats, mode='CMYK')
+feat_img.save('feat.tiff')
+#cat_img.save('cat.tiff')
